35427c8b329e8182d46f5475fac9611f2b2c01ce
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-22 20:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //function calls dwarf names in numbered list
 var dwarf= ["Dopey", "Bashful", "Grumpy"];
 function dwarfRollCall(dwarves) {
@@ -7,6 +8,14 @@ return	(i+1) + `. `  + dwarves[i]+ ` ` + (i+2) + `. ` + dwarves[i+1]+` ` +(i+3)+
 
 //function calls array in all uppcase letters followed by "!"
 var planeteerCalls =("earth", "wind", "fire", "water", "heart");
+=======
+var dwarf= ["Dopey", "Bashful", "Grumpy"];
+function dwarfRollCall(dwarves) {
+
+}
+
+  planeteerCalls =("earth", "wind", "fire", "water", "heart");
+
 function summonCaptainPlanet(planeteerCalls){
 	var planet=[];
 	for(var i=0; i<planeteerCalls.length;i++){
@@ -15,7 +24,10 @@ function summonCaptainPlanet(planeteerCalls){
 return planet;
 }
 
+<<<<<<< HEAD
 //function states true when word is greater than 4 letters
+=======
+>>>>>>> 19983447b7b746a2704702c24c67a476323f7da4
 var words=["earth", "wind", "heart", "fire"];
 function longPlaneteerCalls(words) {
   for(var i=0; i<words.length;i++){
@@ -26,6 +38,7 @@ return false; }
 }
 }
 
+<<<<<<< HEAD
 //function selects cheddar in array of strings, returns "no cheese!" when string is not cheddar
 function findTheCheese(foods) {
   for(var i=0; i<foods.length; i++){
@@ -34,4 +47,12 @@ function findTheCheese(foods) {
 }
   if(foods !== "cheddar"){
   return "no cheese!"; }
+=======
+function findTheCheese (foods) {
+  for(var i=0; i<foods.length;i++)
+  if(foods.length(i)==="cheddar"||"gouda"||"camembert"){
+  return foods[i]}
+  else{
+  return "no cheese!"}
+>>>>>>> 19983447b7b746a2704702c24c67a476323f7da4
   }
